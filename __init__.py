6b10a7c1821a45af800cfea3059dea40d93db8ec
Julia <jula.albert@yahoo.de>
2019-09-25 16:35:56 +0200

    190925 16:35 fix typo and remove old reference code

--- __init__.py
+++ __init__.py
@@ -8,6 +8,7 @@ import requests
 
 __author__ = "Julia Albert"
 
+# Funktion zum Verwendung der API
 def getWeather(location):
     url = "http://api.worldweatheronline.com/premium/v1/weather.ashx?key=985ef9c3a7bd499587c115157192509&q=" + location + "&tp=3&num_of_days=1&cc=yes&format=json"
 
@@ -29,36 +30,8 @@ class WeatherSkill(MycroftSkill):
     # Intent definieren
     def initialize(self):
 
-        # Intent on/off
-        on_off_intent = IntentBuilder("On_Off_Intent").require("action").require("ledName").require("actionName").build()
-        self.register_intent(on_off_intent, self.handle_on_off_intent)
-
         weather_intent = IntentBuilder("Weather_Intent").require("action").require("location").build()
         self.register_intent(weather_intent, self.handle_weather_intent)
-
-    # Intent-handler für An- oder Ausschalten defininieren
-    def handle_on_off_intent(self, message):
-
-        # Variablen an Unserinput (Spracheingabe) anpassen
-        ledMessage = message.data.get("ledName")
-        actionMessage = message.data.get("actionName")
-
-        # Request formulieren
-        led, action = makeRequest(ledMessage, actionMessage)
-
-        # Request(s) senden und Sprachausgabe machen
-        if led == "all":
-            resRed, resGreen, resBred, resBgreen = allLED(action)
-            if resRed.status_code == 200 and resGreen.status_code == 200 and resBred.status_code == 200 and resBgreen.status_code == 200:
-                self.speak_dialog("allOnOff", {"name": ledMessage, "status": actionMessage})
-            else:
-                self.speak_dialog("request.fail")
-        else:
-            res = requestNormal(led, action)
-            if res.status_code == 200:
-                self.speak_dialog("OnOff", {"name": ledMessage, "status": actionMessage})
-            else:
-                self.speak_dialog("request.fail")
 
     # Intent-handler für Wetter
     def handle_weather_intent(self, message):
